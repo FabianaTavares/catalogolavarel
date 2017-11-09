51f20ed7e7ddbc8e5f9fae41a7d819ace8d52a58
Fabiana Tavares <fabi.tavares1@gmail.com>
2017-11-09 11:09:17 -0200

    app
    Adicionando a pasta APP do projeto, ainda aprendendo sobre o github

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Produto;
+use Illuminate\Support\Facades\Auth;
+use Session;
+
+class ProdutosController extends Controller
+{
+    public function index(){
+        $produtos = Produto::paginate(2);
+        return view('produtos.index', array('produtos' => $produtos, 'busca'=>null));
+    }
+
+    public function show($id){
+        $produto = Produto::find($id);
+        return view('produtos.show', array('produto' => $produto));
+    }
+
+    public function create(){
+        if(Auth::check()){
+            return view('produtos.create');
+        }
+        else{
+            return redirect('login');
+        }
+    }
+
+    public function store(Request $request){
+
+        $this->validate($request, [
+            'referencia' => 'required|unique:produtos|min:3',
+            'titulo' => 'required|min:3',
+        ]);
+
+        $produto = new Produto();
+        $produto->referencia = $request->input('referencia');
+        $produto->titulo = $request->input('titulo');
+        $produto->descricao = $request->input('descricao');
+        $produto->preco = $request->input('preco');
+        if($produto->save()){
+            return redirect('produtos');
+        }
+    }
+
+    public function edit($id){
+        if(Auth::check()){
+            $produto = Produto::find($id);
+            return view('produtos.edit', array('produto' => $produto));
+        }
+        else{
+            return redirect('login');
+        }
+    }
+
+    public function update($id, Request $request){
+        if(Auth::check()){
+            $produto = Produto::find($id);
+            $this->validate($request, [
+                'referencia' => 'required|min:3',
+                'titulo' => 'required|min:3',
+            ]);
+
+            if($request->hasFile('fotoproduto')){
+                $imagem = $request->file('fotoproduto');
+                $nomearquivo = md5($id).".".$imagem->getClientOriginalExtension();
+                $request->file('fotoproduto')->move(public_path('./img/produtos/'), $nomearquivo);
+            }
+
+            $produto->referencia = $request->input('referencia');
+            $produto->titulo = $request->input('titulo');
+            $produto->descricao = $request->input('descricao');
+            $produto->preco = $request->input('preco');
+            $produto->save();
+            Session::flash('mensagem', 'Produto alterado com sucesso.');
+            return redirect()->back();
+        }
+        else{
+            return redirect('login');
+        }
+    }
+
+    public function destroy($id){
+        if(Auth::check()){
+            $produto = Produto::find($id);
+            $produto->delete();
+            Session::flash('mensagem', 'Produto excluído com sucesso.');
+            return redirect()->back();
+        }
+        else{
+            return redirect('login');
+        }
+    }
+
+    public function buscar(Request $request){
+        $produtos = Produto::where('titulo', 'LIKE',
+            '%'.$request->input('busca').'%')->orwhere('descricao', 'LIKE',
+            '%'.$request->input('busca').'%')->paginate(2);
+
+        return view('produtos.index', array('produtos'=>$produtos, 'busca'=>$request->input('busca')));
+    }
+
+}
