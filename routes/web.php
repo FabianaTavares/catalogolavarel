772e6c4c2c096b8dc6c3c9ea0f216bcd8a504f24
Fabiana Tavares <fabi.tavares1@gmail.com>
2017-11-09 11:21:53 -0200

    add folder routes of Laravel
    add folder routes of Laravel

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('produtos', 'ProdutosController');
+Route::get('adicionar-produto', 'ProdutosController@create');
+Route::get('produtos/{id}/editar', 'ProdutosController@edit');
+
+Route::post('produtos/buscar', 'ProdutosController@buscar');
+
+
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
