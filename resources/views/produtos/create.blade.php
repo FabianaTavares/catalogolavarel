1cb505bd3632aa9a1e94faebc74caf831bb0cb21
Fabiana Tavares <fabi.tavares1@gmail.com>
2017-11-09 11:21:01 -0200

    pasta resources
    add past resources off Lavarel

---
+++ resources/views/produtos/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('title', 'Adicionar um produto')
+@section('content')
+    <h1>Criar um novo produto</h1>
+    @if(count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    {{Form::open(['action' => 'ProdutosController@store'])}}
+    {{Form::label('referencia', 'Referência')}}
+    {{Form::text('referencia', '', ['class' => 'form-control', 'required', 'placeholder' => 'Referência'])}}
+    {{Form::label('titulo', 'Titulo')}}
+    {{Form::text('titulo', '', ['class'=>'form-control', 'required', 'placeholder'=>'Título'])}}
+    {{Form::label('descricao', 'Descrição')}}
+    {{Form::textarea('descricao', '', ['rows'=>3, 'class'=>'form-control', 'required', 'placeholder'=>'Descricao'])}}
+    {{Form::label('preco', 'Preço')}}
+    {{Form::text('preco', '', ['class'=>'form-control', 'required', 'placeholder'=>'Preço'])}}
+    <br/>
+    {{Form::submit('Cadastrar!', ['class'=>'btn btn-default'])}}
+    {{Form::close()}}
+@endsection
